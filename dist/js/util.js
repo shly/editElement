e05f9adb0878b9047d4faafef8a29db1338204eb
shanglingyue <shanglingyue@utry.cn>
2018-01-18 17:35:45 +0800

    待优化

--- dist/js/util.js
+++ dist/js/util.js
@@ -1,34 +1,139 @@
 'use strict';
 
-function createAnElement(name, parent) {
+var createElementUtil = {};
+!function () {
+  var pos = {
+    'x': '0px',
+    'y': '0px'
+  };
   var newObj = {
     element: {},
     style: {}
   };
   var wrapper = document.createElement('div');
   $(wrapper).addClass('wrapper');
-  var ele = document.createElement(name);
-  // $(ele).css({
-  //   'background': 'blue',
-  //   'position': 'absolute',
-  //   'left': '0',
-  //   'top': '0',
-  //   'right': '0',
-  //   'bottom': '0',
-  //   'margin': 'auto',
-  //   'width': '100px',
-  //   'height': '100px'
-  // })
-  var operate = document.createElement('div');
-  $(wrapper).append(ele);
-  var operateContent = createOperationNode();
-  $(wrapper).append($(operateContent));
-  parent.append(wrapper);
-  newObj.element = ele;
-  newObj.style = wrapper.style;
-  return newObj;
-}
-function createOperationNode() {
-  var operateContent = '<div class="operate">\n      <div class="circleArea tl"></div>\n      <div class="circleArea bl"></div>\n      <div class="circleArea tr"></div>\n      <div class="circleArea br"></div>\n      <div class="rectangleArea t"></div>\n      <div class="rectangleArea l"></div>\n      <div class="rectangleArea r"></div>\n      <div class="rectangleArea b"></div>\n       </div> ';
-  return operateContent;
-}
+  var ele = {};
+
+  var MouseUtil = {
+    onMouseDown: function onMouseDown(e) {
+      pos = {
+        x: e.clientX,
+        y: e.clientY
+      };
+    },
+    onMouseMove: function onMouseMove(event) {
+      var newPos = {
+        x: event.clientX,
+        y: event.clientY
+      };
+      var width = pos.x - newPos.x;
+      var height = pos.y - newPos.y;
+      resetPosition(width, height);
+      pos = JSON.parse(JSON.stringify(newPos));
+    },
+    onMouseUp: function onMouseUp(e) {
+      $(e.target).off('mousemove');
+      $(e.target).off('mouseup');
+    }
+  };
+  createElementUtil = {
+    createAnElement: function createAnElement(name, parent) {
+      var newElement = document.createElement(name);
+      $(wrapper).append(newElement);
+      var operateContent = createOperationNode();
+      $(wrapper).append($(operateContent));
+      parent.append(wrapper);
+      $('.operate .circleArea').on('mousedown', function (event) {
+        ele = $(event.target);
+        MouseUtil.onMouseDown(event);
+        ele.on('mousemove', function (event) {
+          MouseUtil.onMouseMove(event);
+        });
+        ele.on('mouseup', function (event) {
+          MouseUtil.onMouseUp(event);
+        });
+        ele.on('mouseleave', function (event) {
+          MouseUtil.onMouseUp(event);
+        });
+      });
+      $('.operate .rectangleArea').on('mousedown', function (event) {
+        ele = $(event.target);
+        MouseUtil.onMouseDown(event);
+        ele.on('mousemove', function (event) {
+          MouseUtil.onMouseMove(event);
+        });
+        ele.on('mouseup', function (event) {
+          MouseUtil.onMouseUp(event);
+        });
+        ele.on('mouseleave', function (event) {
+          MouseUtil.onMouseUp(event);
+        });
+      });
+      newObj.element = ele;
+      newObj.style = wrapper.style;
+      return newObj;
+    }
+  };
+  function createOperationNode() {
+    var operateContent = '<div class="operate">\n        <div class="circleArea tl"></div>\n        <div class="circleArea bl"></div>\n        <div class="circleArea tr"></div>\n        <div class="circleArea br"></div>\n        <div class="rectangleArea t"></div>\n        <div class="rectangleArea l"></div>\n        <div class="rectangleArea r"></div>\n        <div class="rectangleArea b"></div>\n         </div> ';
+    return operateContent;
+  }
+  function resetPosition(width, height) {
+    var eleClass = ele.attr('class').split(' ')[1];
+    if (eleClass === 'tl') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) + height + 'px',
+        'width': parseInt($(wrapper).css('width')) + width + 'px',
+        'top': parseInt($(wrapper).css('top')) - height + 'px',
+        'left': parseInt($(wrapper).css('left')) - width + 'px'
+      });
+    } else if (eleClass === 'tr') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) + height + 'px',
+        'width': parseInt($(wrapper).css('width')) - width + 'px',
+        'top': parseInt($(wrapper).css('top')) - height + 'px'
+      });
+    } else if (eleClass === 'bl') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) - height + 'px',
+        'width': parseInt($(wrapper).css('width')) + width + 'px',
+        'left': parseInt($(wrapper).css('left')) - width + 'px'
+      });
+    } else if (eleClass === 'br') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) - height + 'px',
+        'width': parseInt($(wrapper).css('width')) - width + 'px'
+      });
+    } else if (eleClass === 't') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) + height + 'px',
+        'top': parseInt($(wrapper).css('top')) - height + 'px'
+      });
+    } else if (eleClass === 'b') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) - height + 'px'
+      });
+    } else if (eleClass === 'l') {
+      $(wrapper).css({
+        'width': parseInt($(wrapper).css('width')) + width + 'px',
+        'left': parseInt($(wrapper).css('left')) - width + 'px'
+      });
+    } else if (eleClass === 'r') {
+      $(wrapper).css({
+        'width': parseInt($(wrapper).css('width')) - width + 'px'
+      });
+    }
+    $('.t').css({
+      'left': parseInt($(wrapper).css('width')) / 2 - parseInt($('.t').css('width')) / 2
+    });
+    $('.b').css({
+      'left': parseInt($(wrapper).css('width')) / 2 - parseInt($('.t').css('width')) / 2
+    });
+    $('.l').css({
+      'top': parseInt($(wrapper).css('height')) / 2 - parseInt($('.t').css('height')) / 2
+    });
+    $('.r').css({
+      'top': parseInt($(wrapper).css('height')) / 2 - parseInt($('.t').css('height')) / 2
+    });
+  }
+}();
--- src/js/util.js
+++ src/js/util.js
@@ -1,41 +1,146 @@
-function createAnElement(name, parent) {
-  const newObj = {
+var createElementUtil = {}
+!function() {
+  var pos = {
+    'x': '0px',
+    'y': '0px'
+  }
+  let newObj = {
     element: {},
     style: {}
   }
-  const wrapper = document.createElement('div')
+  let wrapper = document.createElement('div')
   $(wrapper).addClass('wrapper')
-  const ele = document.createElement(name)
-  // $(ele).css({
-  //   'background': 'blue',
-  //   'position': 'absolute',
-  //   'left': '0',
-  //   'top': '0',
-  //   'right': '0',
-  //   'bottom': '0',
-  //   'margin': 'auto',
-  //   'width': '100px',
-  //   'height': '100px'
-  // })
-  const operate = document.createElement('div')
-  $(wrapper).append(ele)
-  const operateContent = createOperationNode()
-  $(wrapper).append($(operateContent))
-  parent.append(wrapper)
-  newObj.element = ele
-  newObj.style = wrapper.style
-  return newObj
-}
-function createOperationNode() {
-  var operateContent = `<div class="operate">
-      <div class="circleArea tl"></div>
-      <div class="circleArea bl"></div>
-      <div class="circleArea tr"></div>
-      <div class="circleArea br"></div>
-      <div class="rectangleArea t"></div>
-      <div class="rectangleArea l"></div>
-      <div class="rectangleArea r"></div>
-      <div class="rectangleArea b"></div>
-       </div> `
-  return operateContent
-}
+  let ele = {}
+ 
+  var MouseUtil = {
+    onMouseDown: function(e) {
+      pos = {
+        x: e.clientX,
+        y: e.clientY
+      }
+    },
+    onMouseMove: function (event) {
+      let newPos = {
+        x: event.clientX,
+        y: event.clientY
+      }
+      let width = pos.x - newPos.x
+      let height = pos.y - newPos.y
+      resetPosition(width, height)
+      pos = JSON.parse(JSON.stringify(newPos))
+    },
+    onMouseUp: function (e) {
+      $(e.target).off('mousemove')
+      $(e.target).off('mouseup')
+    }
+  }
+  createElementUtil = {
+    createAnElement: function (name, parent) {
+      let newElement = document.createElement(name)
+      $(wrapper).append(newElement)
+      let operateContent = createOperationNode()
+      $(wrapper).append($(operateContent))
+      parent.append(wrapper)
+      $('.operate .circleArea').on('mousedown', function (event) {
+        ele = $(event.target)
+        MouseUtil.onMouseDown(event)
+        ele.on('mousemove', function (event) {
+          MouseUtil.onMouseMove(event)
+        })
+        ele.on('mouseup', function (event) {
+          MouseUtil.onMouseUp(event)
+        })
+        ele.on('mouseleave', function (event) {
+          MouseUtil.onMouseUp(event)
+        })
+      })
+      $('.operate .rectangleArea').on('mousedown', function (event) {
+        ele = $(event.target)
+        MouseUtil.onMouseDown(event)
+        ele.on('mousemove', function (event) {
+          MouseUtil.onMouseMove(event)
+        })
+        ele.on('mouseup', function (event) {
+          MouseUtil.onMouseUp(event)
+        })
+        ele.on('mouseleave', function (event) {
+          MouseUtil.onMouseUp(event)
+        })
+      })
+      newObj.element = ele
+      newObj.style = wrapper.style
+      return newObj
+    }
+  }
+  function createOperationNode() {
+    var operateContent = `<div class="operate">
+        <div class="circleArea tl"></div>
+        <div class="circleArea bl"></div>
+        <div class="circleArea tr"></div>
+        <div class="circleArea br"></div>
+        <div class="rectangleArea t"></div>
+        <div class="rectangleArea l"></div>
+        <div class="rectangleArea r"></div>
+        <div class="rectangleArea b"></div>
+         </div> `
+    return operateContent
+  }
+  function resetPosition(width, height) {
+    let eleClass = ele.attr('class').split(' ')[1]
+    if (eleClass === 'tl') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) + height + 'px',
+        'width': parseInt($(wrapper).css('width')) + width + 'px',
+        'top': parseInt($(wrapper).css('top')) - height + 'px',
+        'left': parseInt($(wrapper).css('left')) - width + 'px',
+      })
+    } else if (eleClass === 'tr') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) + height + 'px',
+        'width': parseInt($(wrapper).css('width')) - width + 'px',
+        'top': parseInt($(wrapper).css('top')) - height + 'px'
+      })
+    } else if (eleClass === 'bl') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) - height + 'px',
+        'width': parseInt($(wrapper).css('width')) + width + 'px',
+        'left': parseInt($(wrapper).css('left')) - width + 'px',
+      })
+    } else if (eleClass === 'br') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) - height + 'px',
+        'width': parseInt($(wrapper).css('width')) - width + 'px',
+      })
+    } else if (eleClass === 't') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) + height + 'px',
+        'top': parseInt($(wrapper).css('top')) - height + 'px',
+      })
+    } else if (eleClass === 'b') {
+      $(wrapper).css({
+        'height': parseInt($(wrapper).css('height')) - height + 'px',
+      })
+    } else if (eleClass === 'l') {
+      $(wrapper).css({
+        'width': parseInt($(wrapper).css('width')) + width + 'px',
+        'left': parseInt($(wrapper).css('left')) - width + 'px',
+      })
+    } else if (eleClass === 'r') {
+      $(wrapper).css({
+        'width': parseInt($(wrapper).css('width')) - width + 'px',
+      })
+    }
+    $('.t').css({
+      'left': parseInt($(wrapper).css('width')) / 2 - parseInt($('.t').css('width')) / 2
+    })
+    $('.b').css({
+      'left': parseInt($(wrapper).css('width')) / 2 - parseInt($('.t').css('width')) / 2
+    })
+    $('.l').css({
+      'top': parseInt($(wrapper).css('height')) / 2 - parseInt($('.t').css('height')) / 2
+    })
+    $('.r').css({
+      'top': parseInt($(wrapper).css('height')) / 2 - parseInt($('.t').css('height')) / 2
+    })
+  }
+}()
